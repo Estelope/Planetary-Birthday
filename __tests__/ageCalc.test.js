46106ebae2c93f6c87b42ee3beba9c5d37ca5f8b
Estelope <elopez14567@gmail.com>
2023-07-27 21:28:49 -0600

    2nd test red phase, rename variable 'age'

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -2,10 +2,16 @@ import intergalacticAgeCalc from '../src/intergalacticAgeCalc';
 
 describe('intergalacticAgeCalc', () => {
 
-  test('should succesfully create an age object from input ', () => {
+  test('it should succesfully create an age object from input ', () => {
     const ageCalc = new intergalacticAgeCalc(45);
-    expect(ageCalc.age).toEqual(45);
-  }
-  )
+    expect(ageCalc.earthAge).toEqual(45);
+  });
+
+  test('it should calculate age in Mercury years', () => {
+    const ageCalc = new intergalacticAgeCalc(45);
+    expect(ageCalc.calculateMercuryAge()).toBeCloseTo(187, 3);
+  });
+
+
 });
 
